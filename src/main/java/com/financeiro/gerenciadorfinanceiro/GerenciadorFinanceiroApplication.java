334f35c5706d0389c984b4eafc8df9660bbf0941
tiagoAlmeidaLl <tlpalmeida@hotmail.com>
2023-05-22 10:56:59 -0300

    Inicialização do projeto

---
+++ src/main/java/com/financeiro/gerenciadorfinanceiro/GerenciadorFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.financeiro.gerenciadorfinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorFinanceiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorFinanceiroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/financeiro/gerenciadorfinanceiro/GerenciadorFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeiro.gerenciadorfinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorFinanceiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
